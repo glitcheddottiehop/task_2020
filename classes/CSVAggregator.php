2868b8ad644baef0ce78a60f2f37094fdd5495a9
glitcheddottiehop <smadzenites@gmail.com>
2020-09-30 23:18:22 +0300

    Turn CSVAggregator into class

--- classes/CSVAggregator.php
+++ classes/CSVAggregator.php
@@ -5,65 +5,80 @@ include 'Connection.php';
 // Turn off all error reporting
 error_reporting(0);
 
-$dir    = '../data';
-$files = scandir($dir);
+class CSVAggregator extends Connection 
+{
+    public function getFiles()
+    {
+        $dir    = '../data';
+        $files = scandir($dir);
+        $refactoredList = [];
 
-$refactoredList = [];
-foreach ($files as $file){
-    if (strpos($file, '.csv') !== false) {
-        $refactoredList[] = $file;
+        foreach ($files as $file){
+            if (strpos($file, '.csv') !== false) {
+                $refactoredList[] = $file;
+            }
+        }
+        return $this->CSVToArray($refactoredList);
+    }
+
+    public function CSVToArray($list)
+    {
+        foreach ($list as $item) {
+            $file ="../data/$item";
+            $csv = file_get_contents($file);
+            $array = array_map("str_getcsv", explode("\n", $csv));
+            $json = json_encode($array);
+
+            $datas = [];
+            $column_names = [];
+            $data = '';  
+
+            foreach ($array[0] as $single_csv) {
+                $column_names[] = $single_csv;
+            }
+
+            foreach ($array as $key => $csv) {
+                if ($key === 0) {
+                    continue;
+                }   
+                foreach ($column_names as $column_key => $column_name) {
+                    $datas[$key][$column_name] = $csv[$column_key]; 
+                }
+            }
+            foreach($datas as $entry) {
+                $columns = array_keys($entry);    
+                $columnName = '';
+                foreach($columns as $column){
+                    $columnName .= $column . ', ';    
+                    $data .= "'" . $entry[$column] . "', ";
+                }
+                $formatValues = substr($data, 0, -2) . ')';
+
+                return $this->createSQL($columnName, $formatValues);
+            }
+        }
+    }
+
+    public function createSQL($columns, $values)
+    {
+        $sql = "";
+        $sql = "INSERT INTO customer_information (" . substr($columns, 0, strpos($columns, ',', -2)) .  ") VALUES ($values";
+        return $this->insert($sql);
+    }
+
+    public function insert($sql)
+    {
+
+        //TODO return all id's of inserted rows
+
+        // return $this->connect()->query("INSERT INTO customer_information (name, surname, email, address, city, gender, soc_security_num,balance) VALUES ('', '', '', '', '', '', '', '')");
+        $this->connect()->query($sql);
+        // return $this->pdo->errorCode();
+        // return $this->pdo->lastInsertId();
+        // ->execute() returns true on success. 
+        // ->errorCode() which you can check for errors.
     }
 }
 
-
-foreach ($refactoredList as $item){
-    $file ="../data/$item";
-    $csv = file_get_contents($file);
-    $array = array_map("str_getcsv", explode("\n", $csv));
-    $json = json_encode($array);
-
-    $datas = [];
-    $column_names = [];
-
-    foreach ($array[0] as $single_csv) {
-    $column_names[] = $single_csv;
-    }
-
-    foreach ($array as $key => $csv) {
-        if ($key === 0) {
-            continue;
-        }   
-
-        foreach ($column_names as $column_key => $column_name) {
-            $datas[$key][$column_name] = $csv[$column_key]; 
-        }
-    }
-
-        
-   
-    $count = count($datas);
-    $data = '';
-    foreach($datas as $entry){
-        $columns = array_keys($entry);    
-        $columnName = '';
-        
-        foreach($columns as $column){
-            $columnName .= $column . ', ';
-            
-        }
-        
-        foreach($entry as $key => $value){
-            if($count == 1){
-                $data .= "'" . $value . "', ";
-            } else {
-
-            }
-           
-        }
-        $sql = "";
-        $sql .= "INSERT INTO customer_information (" . substr($columnName, 0, strpos($columnName, ',', -2)) .  ") VALUES ($data)";
-        
-    }
-   var_dump($sql);
-   print_r($datas);
-} 
+$csvAggregator = new CSVAggregator();
+echo $csvAggregator->getFiles();
--- classes/CustomerInformation.php
+++ classes/CustomerInformation.php
@@ -6,8 +6,6 @@ $dataObj = new GetData();
 
 $data = $dataObj->getCustomerInformation((isset($_GET['page']) ? ($_GET['page'] == 1 ? 0 : $_GET['page']) : 0), (isset($_GET['per_page']) ? $_GET['per_page'] : 10));
 
-// echo $_GET['page'];
-
 foreach ($data as $row){
     if ($row['gender'] == 'male') {
         $row['gender'] = 'mr';
@@ -16,7 +14,6 @@ foreach ($data as $row){
     }
     
     $row['balance'] = $row['balance'] * 100;
-
 
     if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i",$row['email'])) {
         $row['email'] = null;
