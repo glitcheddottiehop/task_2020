ede0533c117a1332bdd56f417be08cc72f366f6c
glitcheddottiehop <smadzenites@gmail.com>
2020-10-03 20:55:44 +0300

    Turn file into class

--- classes/CustomerInformation.php
+++ classes/CustomerInformation.php
@@ -1,24 +1,44 @@
 <?php 
 
-include 'GetData.php';
+include 'Data.php';
 
-$dataObj = new GetData();
+// Turn off all error reporting
+error_reporting(0);
 
-$data = $dataObj->getCustomerInformation((isset($_GET['page']) ? ($_GET['page'] == 1 ? 0 : $_GET['page']) : 0), (isset($_GET['per_page']) ? $_GET['per_page'] : 10));
+/**
+ * Class CustomerInformation
+ * 
+ * Displays fetched data
+ */
+class CustomerInformation extends Data 
+{
+     /**
+     * Modify fetched data before displaying
+     */
+    public function getCustomerInformation()    
+    {
+        $page = (isset($_GET['page']) ? ($_GET['page'] == 1 ? 0 : $_GET['page']) : 0);
+        $perPage = (isset($_GET['per_page']) ? $_GET['per_page'] : 10);
+        $data = $this->getData($page, $perPage);
+        
+        foreach ($data as $row){
+            if ($row['gender'] == 'male') {
+                $row['gender'] = 'mr';
+            }else if ($row['gender'] == 'female') {
+                $row['gender'] = 'ms';
+            }
 
-foreach ($data as $row){
-    if ($row['gender'] == 'male') {
-        $row['gender'] = 'mr';
-    }else if ($row['gender'] == 'female') {
-        $row['gender'] = 'ms';
+            $row['balance'] = $row['balance'] * 100;
+        
+            if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i",$row['email'])) {
+                $row['email'] = null;
+            }
+        
+            $json = json_encode($row);
+            echo $json;
+        }
     }
-    
-    $row['balance'] = $row['balance'] * 100;
-
-    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i",$row['email'])) {
-        $row['email'] = null;
-    }
-
-    $json = json_encode($row);
-    echo $json;
 }
+
+$customerInformation = new CustomerInformation();
+echo $customerInformation->getCustomerInformation();
